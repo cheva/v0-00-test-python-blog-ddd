4573894fc533c5d5702ddff8df2601f8243d1e19
Dee <dee.covan@gmail.com>
2024-10-22 15:48:15 +0300

    @TODO use mailer as Infrastructure Feature (service)

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,6 +18,9 @@ login.login_view = "login"
 
 if not app.debug:
     if app.config["MAIL_SERVER"]:
+        # Mailer
+        # @TODO deploy https://github.com/railsware/mailtrap-python?tab=readme-ov-file
+        # @TODO use mailer as Infrastructure Feature (service)
         auth = None
         if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
             auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
@@ -33,5 +37,23 @@ if not app.debug:
         )
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
+        # Logger
+        # @TODO use logger as Application Feature (service)
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
+            file_handler = RotatingFileHandler(
+                "logs/microblog.log", maxBytes=10240, backupCount=10
+            )
+            file_handler.setFormatter(
+                logging.Formatter(
+                    "%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]"
+                )
+            )
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+            app.logger.setLevel(logging.INFO)
+            app.logger.info("Microblog startup")
 
 from app import routes, models, errors
