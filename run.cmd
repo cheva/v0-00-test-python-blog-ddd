02db03fd76e9f6c84c06f05c7dacaee0b00fc40b
Dee <dee.covan@gmail.com>
2024-10-22 15:38:54 +0300

    @TODO deploy https://github.com/railsware/mailtrap-python?tab readme-ov-file

--- run.cmd
+++ run.cmd
@@ -1,12 +1,9 @@
 set FLASK_APP=microblog.py
 
+@rem @TODO deploy https://github.com/railsware/mailtrap-python?tab=readme-ov-file
+
 set MAIL_SERVER=localhost
 set MAIL_PORT=8025
-set MAIL_SERVER=smtp.googlemail.com
-set MAIL_PORT=587
-set MAIL_USE_TLS=1
-set MAIL_USERNAME=%1
-set MAIL_PASSWORD=%2
-
 start aiosmtpd -n -c aiosmtpd.handlers.Debugging -l localhost:8025
+
 start flask run --port 88
