6c600259b3b5800ecc5318dbe30b7c1d03d4539c
Dee <dee.covan@gmail.com>
2024-10-24 11:53:21 +0300

    Fix code for Chapter 13 Start chapter 14

--- config.py
+++ config.py
@@ -12,4 +12,5 @@ class Config:
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['dmytro.cheva@gmail.com']
-    POSTS_PER_PAGE = 25
+    POSTS_PER_PAGE = 5
+    LANGUAGES = ['en', 'uk']
