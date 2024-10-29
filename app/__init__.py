1c639f2e71552830104243e13c26d6d673c1b4b7
Dee <dee.covan@gmail.com>
2024-10-29 11:33:25 +0200

    Start refactore Move templates Add blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,12 @@ mail = Mail(app)
 moment = Moment(app)
 babel = Babel(app, locale_selector=get_locale)
 
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
+
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -55,4 +61,4 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 
-from app import routes, models, errors
+from app import routes, models
